33407b94861fd1a44244cb925ddc0a0009c8a54d
Carlos Alberto <ccarloos9@gmail.com>
2020-10-08 20:33:10 -0300

    env

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -23,6 +23,10 @@ module.exports = {
     const { email, senha } = request.body;
     const userValidate = await User.findOne({ email, senha });
 
+    if (!userValidate) {
+      return response.status(401).json({ Error: 'Usuario nao encontrado' });
+    }
+
     return response.json(userValidate);
   },
 };
