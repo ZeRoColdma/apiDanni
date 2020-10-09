ec482fa3b4ff535d2803213d2534387eb25395f9
Carlos <carlos.alberto.junior1@vale.com>
2020-10-09 13:27:18 -0300

    09/10/2020

--- src/controllers/ProdutosController.js
+++ src/controllers/ProdutosController.js
@@ -17,4 +17,6 @@ module.exports = {
 
     return response.json(data);
   },
+
+  async delete(request, response) {},
 };
--- src/models/Produtos.js
+++ src/models/Produtos.js
@@ -6,27 +6,29 @@ const ProdutosSchema = mongoose.Schema(
       type: [String],
       require: true,
     },
+
     quantidade: {
       type: Number,
       require: true,
     },
+
     promocao: {
       type: Boolean,
       require: true,
     },
+
+    dataEstoque: {
+      type: [String],
+    },
+
     cliente: {
       type: String,
     },
+
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
     },
-    dataEstoque: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'product',
-      },
-    ],
   },
   { timestamps: true },
 );
