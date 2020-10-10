83d1328e4d5323863df7a14eec51db970087a1c7
Carlos Alberto <ccarloos9@gmail.com>
2020-10-10 11:09:49 -0300

    Mudança ref

--- src/models/Produtos.js
+++ src/models/Produtos.js
@@ -29,15 +29,12 @@ const ProdutosSchema = mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
     },
-<<<<<<< HEAD
     dataEstoque: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
       },
     ],
-=======
->>>>>>> ec482fa3b4ff535d2803213d2534387eb25395f9
   },
   { timestamps: true },
 );
